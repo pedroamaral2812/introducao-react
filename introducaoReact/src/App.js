73dfb2c205bfbcf566143e9632b99b94cf416e95
Pedro Amaral <suporte@m2corp.com.br>
2019-08-26 15:57:31 -0300

    Class Componente

--- introducaoReact/src/App.js
+++ introducaoReact/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./App.css";
+
+import TechList from "./components/TechList";
 
 function App() {
-  return <h1>Hello ROcketseat</h1>;
+  return <TechList />;
 }
 
 export default App;
---
+++ introducaoReact/src/components/TechList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  state = {};
+  render() {
+    return (
+      <ul>
+        <li>NOde.js</li>
+        <li>React Js</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
